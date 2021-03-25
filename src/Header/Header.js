9cde9d008d78642396eaf7692ec86069c9ddf9a0
Zhi Thon <e0176351@u.nus.edu>
2021-03-25 23:56:06 +0800

    Simple headers design

---
+++ src/Header/Header.js
@@ -0,0 +1,24 @@
+import './Header.css'
+
+function Header() {
+    return (
+        <div className="header-row">
+            <div className="header-buttons">
+                <button className="buttons">
+                    Education and Projects
+            </button>
+                <button className="buttons">
+                    Work Experience
+            </button>
+                <button className="buttons">
+                    Personal Projects
+            </button>
+                <button className="buttons">
+                    About Me
+            </button>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
