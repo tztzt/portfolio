d9a0a8d210c7c2c0c93ed5b240e5831d200d3012
Zhi Thon <e0176351@u.nus.edu>
2021-03-31 01:25:24 +0800

    small about me update

--- src/About/About.tsx
+++ src/About/About.tsx
@@ -2,8 +2,20 @@ import './About.css'
 
 function About() {
     return (
-        <div className="content">
-            About Me Contents
+        <div>
+            <div className="row">
+                <div className="display-pic">
+                    <img className="picture" src={process.env.PUBLIC_URL + '/photo1.jpg'} alt="image" />
+                </div>
+                <div className="about-content">
+                    Name: Zhi Thon <br />
+                Major: Computer Engineering (Bachelor of Engineering) <br />
+                Graduating: August 2021
+            </div>
+            </div>
+            <div className="about-mumbo">
+                Unfortunately, this is an ongoing mini project by me and still looks simple and unpolished, for now.
+            </div>
         </div>
     );
 }
